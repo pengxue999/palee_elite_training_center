04da06ea98335f638f1c62a45ebc95be8c8f4a8f
pengxue999 <pengxuechongvue@gmail.com>
2026-04-18 16:40:19 +0700

    commit production

--- lib/core/constants/constant.dart
+++ lib/core/constants/constant.dart
@@ -1,5 +1,6 @@
 class AppConstants {
-  static const String baseUrl = "http://127.0.0.1:8000";
+  static const String baseUrl =
+      "https://paleeapiservice-production.up.railway.app";
   static const String iconPath = "assets/icons";
   static const String imagePath = "assets/images";
 }
